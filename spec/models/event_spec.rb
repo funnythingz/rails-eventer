cb6cc100636ab5dc0f5f1fde92ee7bb4c4be11c1
funnythingz <funnythingz69+github@gmail.com>
2014-10-14 23:32:26 +0900

    modified models/event_spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,14 +1,13 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Event do
   describe '#name' do
     context 'is empty' do
+      let(:event) { Event.new(name: '') }
       it 'invalid' do
-        event = Event.new(name: nil)
         event.valid?
         expect(event.errors[:name]).to be_present
       end
     end
   end
-
 end
