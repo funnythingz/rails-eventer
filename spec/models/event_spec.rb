114d40365e7f52cbaa0c057b8e4cab800a04f717
funnythingz <funnythingz69+github@gmail.com>
2014-10-14 23:43:32 +0900

    refactor models/event_spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,44 +2,7 @@ require 'rails_helper'
 
 describe Event do
   describe '#name' do
-    context 'when is nil' do
-      let(:event) { Event.new(name: nil) }
-      it 'invalid' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
-
-    context 'when is empty' do
-      let(:event) { Event.new(name: '') }
-      it 'invalid' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
-
-    context 'when is "Hello world"' do
-      let(:event) { Event.new(name: 'Hello world') }
-      it 'valid' do
-        event.valid?
-        expect(event.errors[:name]).to be_blank
-      end
-    end
-
-    context 'when string length is 50 count' do
-      let(:event) { Event.new(name: 'a' * 50) }
-      it 'valid' do
-        event.valid?
-        expect(event.errors[:name]).to be_blank
-      end
-    end
-
-    context 'when string length over 50 count' do
-      let(:event) { Event.new(name: 'a' * 51) }
-      it 'valid' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
+    it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_most(50) }
   end
 end
