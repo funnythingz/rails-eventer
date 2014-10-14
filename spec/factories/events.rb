4942efd42aa607ff7827dbfdeb677810d383c4c2
funnythingz <funnythingz69+github@gmail.com>
2014-10-14 23:51:41 +0900

    added factories/events

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,12 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
   factory :event do
+    owner
+    name { Faker::Lorem.sentence }
+    place { Faker::Address.city }
+    content { Faker::Lorem.paragraph }
+    start_time { rand(1..3).days.from_now }
+    end_time { start_time + rand(1..30).hours }
   end
 end
