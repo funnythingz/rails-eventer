360705e582663e607328a92ec40513f2daa59455
funnythingz <funnythingz69+github@gmail.com>
2014-10-14 23:19:53 +0900

    added models/event_spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Event, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Event do
+  describe '#name' do
+    context 'is empty' do
+      it 'invalid' do
+        event = Event.new(name: nil)
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+  end
+
 end
